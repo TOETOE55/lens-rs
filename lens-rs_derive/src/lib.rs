3c19d0d2516dd0ca128972bcd194da861cf0ae5d
Xyzt Toe <584605539@qq.com>
2021-03-21 21:15:25 +0800

    generate optics in lens_rs::optic centrally delete Optic derive macro version 0.0.9

--- lens-rs_derive/src/lib.rs
+++ lens-rs_derive/src/lib.rs
@@ -1,27 +1,15 @@
 extern crate proc_macro;
-#[macro_use]
-extern crate lazy_static;
-use proc_macro::TokenStream;
-use quote::*;
-use syn::punctuated::Punctuated;
-use syn::Token;
-use syn::{parse_macro_input, Data, DeriveInput, parenthesized};
+use proc_macro::{TokenStream, TokenTree};
 use proc_macro2::Span;
+use quote::*;
+use syn::{
+    parenthesized, parse_macro_input, parse_quote, punctuated::Punctuated, visit::Visit, Data,
+    DeriveInput, ItemEnum, ItemStruct, Token,
+};
 
 use std::collections::HashSet;
-use std::sync::Mutex;
-use syn::parse::{Parse, Result, ParseStream};
-
-lazy_static! {
-    static ref OPTIC_NAMES: Mutex<HashSet<String>> = {
-        let mut m = HashSet::new();
-        m.insert("_Ok".to_string());
-        m.insert("_Err".to_string());
-        m.insert("_Some".to_string());
-        m.insert("_None".to_string());
-        Mutex::new(m)
-    };
-}
+use std::fs;
+use syn::parse::{Parse, ParseStream, Result};
 
 enum OpticMutability {
     Move,
@@ -31,73 +19,73 @@ enum OpticMutability {
 
 impl Parse for OpticMutability {
     fn parse(input: ParseStream) -> Result<Self> {
-        if input.is_empty() { return Ok(Self::Move); }
+        if input.is_empty() {
+            return Ok(Self::Move);
+        }
 
         let content;
 
         parenthesized!(content in input);
         let lookahead = content.lookahead1();
-        println!("{:?}", content);
         if lookahead.peek(Token![mut]) {
             Ok(Self::Mut(content.parse()?))
         } else if lookahead.peek(Token![ref]) {
             Ok(Self::Ref(content.parse()?))
-        } else  {
+        } else {
             Err(input.error("only allow #[optic], #[optic(mut)] or #[optic(ref)] here"))
         }
     }
 }
 
-
-#[proc_macro_derive(Optic, attributes(optic))]
-pub fn derive_optic(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let derive_input = parse_macro_input!(input as DeriveInput);
-    let optics = match derive_input.data {
-        Data::Enum(e) => e
-            .variants
-            .iter()
-            .filter(|var| {
-                var
-                    .attrs
-                    .iter()
-                    .any(|attr| attr.path.is_ident(&syn::Ident::new("optic", Span::call_site())))
-            })
-            .flat_map(|x| {
-                let optic_name = format_ident!("_{}", x.ident);
-                let mut table = OPTIC_NAMES.lock().unwrap();
-                if table.contains(&optic_name.clone().to_string()) { return quote! {}; }
-                table.insert(optic_name.clone().to_string());
-                quote! {
-                    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-                    pub struct #optic_name<Optic>(pub Optic);
-                }
-            })
-            .collect(),
-        Data::Struct(st) => st
-            .fields
-            .iter()
-            .filter(|var| {
-                var
-                    .attrs
-                    .iter()
-                    .any(|attr| attr.path.is_ident(&syn::Ident::new("optic", Span::call_site())))
-            })
-            .flat_map(|x| {
-                let optic_name = format_ident!("_{}", x.ident.as_ref()?);
-                let mut table = OPTIC_NAMES.lock().unwrap();
-                if table.contains(&optic_name.clone().to_string()) { return None; }
-                table.insert(optic_name.clone().to_string());
-                Some(quote! {
-                    #[derive(Copy, Clone)]
-                    #[allow(non_camel_case_types)]
-                    pub struct #optic_name<Optic>(pub Optic);
-                })
-            })
-            .collect(),
-        _ => quote! {},
-    };
-    TokenStream::from(optics)
-}
+// #[proc_macro_derive(Optic, attributes(optic))]
+// pub fn derive_optic(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+//     let derive_input = parse_macro_input!(input as DeriveInput);
+//     let optics = match derive_input.data {
+//         Data::Enum(e) => e
+//             .variants
+//             .iter()
+//             .filter(|var| {
+//                 var
+//                     .attrs
+//                     .iter()
+//                     .any(|attr| attr.path.is_ident(&syn::Ident::new("optic", Span::call_site())))
+//             })
+//             .flat_map(|x| {
+//                 let optic_name = format_ident!("_{}", x.ident);
+//                 let mut table = OPTIC_NAMES.lock().unwrap();
+//                 if table.contains(&optic_name.clone().to_string()) { return quote! {}; }
+//                 table.insert(optic_name.clone().to_string());
+//                 quote! {
+//                     #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+//                     pub struct #optic_name<Optic>(pub Optic);
+//                 }
+//             })
+//             .collect(),
+//         Data::Struct(st) => st
+//             .fields
+//             .iter()
+//             .filter(|var| {
+//                 var
+//                     .attrs
+//                     .iter()
+//                     .any(|attr| attr.path.is_ident(&syn::Ident::new("optic", Span::call_site())))
+//             })
+//             .flat_map(|x| {
+//                 let optic_name = format_ident!("_{}", x.ident.as_ref()?);
+//                 let mut table = OPTIC_NAMES.lock().unwrap();
+//                 if table.contains(&optic_name.clone().to_string()) { return None; }
+//                 table.insert(optic_name.clone().to_string());
+//                 Some(quote! {
+//                     #[derive(Copy, Clone)]
+//                     #[allow(non_camel_case_types)]
+//                     pub struct #optic_name<Optic>(pub Optic);
+//                 })
+//             })
+//             .collect(),
+//         _ => quote! {},
+//     };
+//     TokenStream::from(optics)
+// }
 
 #[proc_macro_derive(Review, attributes(optic))]
 pub fn derive_review(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -607,4 +595,104 @@ pub fn derive_lens(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     };
 
     TokenStream::from(lens)
+}
+
+struct OpticCollector<'a>(&'a mut OpticMap);
+
+impl<'a> OpticCollector<'a> {
+    fn collect_optic_fields<'f>(&mut self, fields: impl Iterator<Item = &'f syn::Field>) {
+        fields.for_each(|field| {
+            if field.attrs.iter().any(|attr| {
+                attr.path
+                    .is_ident(&syn::Ident::new("optic", Span::call_site()))
+            }) {
+                field
+                    .ident
+                    .as_ref()
+                    .map(|ident| format!("_{}", ident.to_string()))
+                    .map(|optic_name| self.0.insert(optic_name));
+            }
+        });
+    }
+}
+
+impl<'a> Visit<'_> for OpticCollector<'a> {
+    fn visit_item_struct(&mut self, item_struct: &ItemStruct) {
+        match &item_struct.fields {
+            syn::Fields::Named(fields_named) => {
+                self.collect_optic_fields(fields_named.named.iter())
+            }
+            syn::Fields::Unnamed(fields_unnamed) => {
+                self.collect_optic_fields(fields_unnamed.unnamed.iter())
+            }
+            syn::Fields::Unit => (),
+        }
+    }
+
+    fn visit_item_enum(&mut self, item_enum: &ItemEnum) {
+        item_enum.variants.iter().for_each(|variant| {
+            if variant.attrs.iter().any(|attr| {
+                attr.path
+                    .is_ident(&syn::Ident::new("optic", Span::call_site()))
+            }) {
+                self.0.insert(format!("_{}", variant.ident));
+            }
+        })
+    }
+}
+
+type OpticMap = HashSet<String>;
+
+#[doc(hidden)]
+#[proc_macro]
+pub fn scan_optics_from_source_files(input: TokenStream) -> TokenStream {
+    let mut iter = input.into_iter();
+    let mut optcis_map = OpticMap::new();
+
+    loop {
+        let token_tree = iter.next();
+        match token_tree {
+            Some(TokenTree::Literal(literal)) => {
+                let file_name = literal.to_string();
+                let file_name = file_name.trim_matches('"');
+                let contents =
+                    String::from_utf8(fs::read(std::path::Path::new(file_name)).unwrap()).unwrap();
+                let syntax = syn::parse_file(&contents)
+                    .expect(".rs files should contain valid Rust source code.");
+                OpticCollector(&mut optcis_map).visit_file(&syntax);
+
+                if let Some(token_tree) = iter.next() {
+                    if let TokenTree::Punct(punct) = token_tree {
+                        if punct.to_string() != "," {
+                            panic!(
+                                "scan_optics_from_source_files!(): expect `,`, got `{}`",
+                                punct.to_string()
+                            );
+                        }
+                    }
+                } else {
+                    break;
+                }
+            }
+            None => break,
+            _ => panic!(
+                "scan_optics_from_source_files!(): expect string literal, got `{:?}`",
+                token_tree
+            ),
+        }
+    }
+
+    let mut struct_items = Vec::<ItemStruct>::with_capacity(optcis_map.len());
+
+    for optic_name in optcis_map {
+        let optic_ident = syn::Ident::new(&optic_name, Span::call_site());
+        struct_items.push(parse_quote! {
+
+               #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+               #[allow(non_camel_case_types)]
+               pub struct #optic_ident<Optic>(pub Optic);
+        });
+    }
+
+    quote!( #( #struct_items )* ).into()
 }
