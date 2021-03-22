c7f1028e02856e75fe70eb43442109ef80cea4e5
Xyzt Toe <584605539@qq.com>
2021-03-22 11:57:26 +0800

    update example.rs

--- example.rs
+++ example.rs
@@ -1,49 +1,107 @@
-#![allow(non_camel_case_types)]
 use lens_rs::*;
-use lens_rs_derive::*;
+use Either::*;
 
-#[derive(Optic, Review, Prism, Debug)]
-enum AnEnum<T> {
-    A(T, i32),
-    #[optic] B(T),
+#[derive(Copy, Clone, Debug, Review, Prism)]
+enum Either<L, R> {
+    #[optic]
+    Left(L),
+    #[optic]
+    Right(R),
 }
 
-#[derive(Optic, Lens, Debug)]
-struct Foo {
-    #[optic] a: i32,
-    #[optic] b: i32,
+#[derive(Copy, Clone, Debug, Lens)]
+struct Tuple<A, B>(#[optic] A, #[optic] B);
+
+#[derive(Copy, Clone, Debug, Lens)]
+struct Foo<A, B> {
+    #[optic]
+    a: A,
+    #[optic]
+    b: B,
 }
 
-fn test1() -> Option<()> {
-    let mut nested: Result<Result<_, ()>, ()> = optics!(_Ok._Ok).review((1, 2));
-    *optics!(_Ok._Ok._0).pm_mut(&mut nested)? += 1;
-    assert_eq!(optics!(_Ok._Ok._0).pm(nested)?, 2);
+#[derive(Clone, Debug, Lens)]
+struct Bar {
+    #[optic]
+    a: String,
+    #[optic]
+    c: i32,
+}
+#[derive(Debug, Lens)]
+struct Shit<'a> {
+    #[optic(ref)]
+    a: &'a str,
+    #[optic(mut)]
+    b: &'a mut Vec<i32>,
+}
 
-    let mut x = (1, (2, (3, 4)));
-    *optics!(_1._1._1).view_mut(&mut x) *= 2;
-    assert_eq!(optics!(_1._1._1).view(x), 8);
 
-    let mut x: (_, Result<_, ()>) = (1, Ok((2, 3)));
-    *optics!(_1._Ok._1).pm_mut(&mut x)? *= 2;
-    assert_eq!(optics!(_1._Ok._1).pm(x)?, 6);
+// T may have i32
+fn may_have_i32<T, Pm: Prism<T, To=i32>>(t: &mut T, pm: Pm) {
+    pm.pm_mut(t).map(|x| *x+=1);
+}
 
-    let mut x = (1, vec![Some((2, 3)), None]);
-    optics!(_1.Mapped._Some._0)
+
+fn main() {
+    ////////////////////////////////////////////
+    let mut x = (
+        1,
+        Left(Tuple(
+            vec![
+                Some(Foo {
+                    a: "a".to_string(),
+                    b: 2,
+                }),
+                None,
+                Some(Foo {
+                    a: 'c'.to_string(),
+                    b: 3,
+                }),
+            ],
+            4,
+        )),
+    );
+    optics!(_1.Left._1).pm_mut(&mut x).map(|x| *x *= 2);
+    optics!(_1.Right).pm_mut(&mut x).map(|x: &mut i32| *x *= 2);
+    *optics!(_0).view_mut(&mut x) += 1;
+    optics!(_1.Left._0.Mapped.Some.a)
         .traverse_mut(&mut x)
         .into_iter()
-        .for_each(|i| *i += 1);
-    assert_eq!(optics!(_1.Mapped._Some._0).traverse(x), vec![3]);
+        .for_each(|s| *s = s.to_uppercase());
 
-    Some(())
-}
+    println!("{:?}", x);
 
+    ////////////////////////////////////////////////////////
+    may_have_i32(&mut x, optics!(_0));
+    may_have_i32(&mut x, optics!(_1.Left._1));
+    println!("{:?}", x);
 
-fn test2() -> Option<()> {
-    let x = optics!(_Some._B).review(Foo {
-        a: 3,
-        b: 2,
-    });
-    assert_eq!(optics!(_Some._B._b).pm(x)?, 2);
+    ///////////////////////////////////////////////////////
+    fn with_field<T>(t: T) -> String
+        where
+            field![a]: Lens<T, To=String>
+    {
+        optics!(a).view(t)
+    }
 
-    Some(())
+    let foo = Foo {
+        a: "this is Foo".to_string(),
+        b: ()
+    };
+    let bar = Bar {
+        a: "this is Bar".to_string(),
+        c: 0
+    };
+
+    println!("{}", with_field(foo));
+    println!("{}", with_field(bar));
+
+    ///////////////////////////////////////////////////////
+
+    let foo = Foo {
+        a: &("foo0", Box::new("foo1")),
+        b: ()
+    };
+    let s = optics!(a._ref._1._box).view_ref(&foo);
+    println!("{}", s);
 }
